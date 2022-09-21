8937e0fae0698afbfbbb8979540143a42aa8809c
yy <wmyaoo@outlook.com>
2022-09-21 17:19:25 +0800

    重构1

--- 2_corona_virus_spider.py
+++ 2_corona_virus_spider.py
@@ -2,6 +2,7 @@
 import re
 import requests
 import json
+from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 
@@ -50,11 +51,36 @@ class CoronaVirusSpider(object):
         home_page = self.get_content_from_url(self.home_url)
         last_day_corona_virus = self.parse_home_page(home_page)
         # 保存数据
-        self.save(last_day_corona_virus, './data/corona_virus_spider.json')
+        self.save(last_day_corona_virus, './data/last_day_corona_virus.json')
+
+    def craw_corona_virus(self):
+        """
+        采集从1月23日以来的疫情信息
+        :return:
+        """
+        # 加载各国疫情数据
+        with open('./data/last_day_corona_virus.json') as fp:
+            last_day_corona_virus = json.load(fp)
+        corona_virus = []
+        # print(last_day_corona_virus)
+        for country in tqdm(last_day_corona_virus, '1月23日以来疫情信息'):
+        # for country in last_day_corona_virus:
+            statistics_data_url = country['statisticsData']
+            statistics_data_json_str = self.get_content_from_url(statistics_data_url)
+            # print(statistics_data_json_str)
+            # 把json数据转换为python数据类型保存到列表中
+            statistics_data = json.loads(statistics_data_json_str)['data']
+            # print(statistics_data)
+            for one_day in statistics_data:
+                one_day['provinceName'] = country['provinceName']
+                # one_day['countryShortCode'] = country['countryShortCode']
+            corona_virus.extend(statistics_data)
+
+        self.save(corona_virus, './data/corona_virus.json')
 
     def run(self):
-        self.craw_last_corona_virus()
-
+        # self.craw_last_corona_virus()
+        self.craw_corona_virus()
 
 if __name__ == '__main__':
     spider = CoronaVirusSpider()
